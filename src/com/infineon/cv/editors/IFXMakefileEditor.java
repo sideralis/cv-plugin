c99f8fa773441233bb317a90e79ea253525502ba
bernard gautier4 <bernard.gautier4@gmail.com>
2011-01-14 18:08:25 +0100

    Project definition parsing.

--- src/com/infineon/cv/editors/IFXMakefileEditor.java
+++ src/com/infineon/cv/editors/IFXMakefileEditor.java
@@ -3,6 +3,9 @@
  */
 package com.infineon.cv.editors;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.eclipse.swt.*;
 
 import org.eclipse.core.resources.IFile;
@@ -18,6 +21,8 @@ import org.eclipse.ui.part.MultiPageEditorPart;
 import org.eclipse.cdt.make.internal.ui.editor.*;
 
 import com.infineon.cv.makefile.ParserMakefile;
+import com.infineon.cv.makefile.XMLNode;
+import com.infineon.cv.makefile.XMLParser;
 
 /**
  * @author gautier
@@ -30,6 +35,7 @@ public class IFXMakefileEditor extends MultiPageEditorPart {
 	private ListViewer targetList;
 	private int indexSource;
 	private ParserMakefile parserMakefile;
+	private XMLParser valueMakefile;
 
 	/**
 	 * Constructor
@@ -105,7 +111,19 @@ public class IFXMakefileEditor extends MultiPageEditorPart {
 			System.out.println("ITCM_BASE_ADDRESS=" + parserMakefile.getItcm_base_address().toString());
 			System.out.println("FORBIDDEN_DEFINES=" + parserMakefile.getFobidden_defines().toString());
 		}
-		// Parse 
+		// Parse possible values
+		valueMakefile = new XMLParser("ProjectSetting.xml");
+		{
+			HashMap<String, ArrayList<String>> xml = null;
+			ArrayList<XMLNode> prjNodes = valueMakefile.getXMLNodes();
+			for (XMLNode node : prjNodes) {
+				if (parserMakefile.getArch().contains(node.getProjectType().trim())) {
+					xml = node.getAttributs();
+					break;
+				}
+			}
+		}
+		
 	}
 	@Override
 	public void setFocus() {
