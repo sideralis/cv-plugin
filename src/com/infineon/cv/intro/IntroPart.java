ba2eb5686bc65bde6beceaba01b93e648882d9e0
bernard.gautier4 <bernard.gautier4@localhost>
2011-12-02 16:37:08 +0100

    Intro screen (not yet working)

---
+++ src/com/infineon/cv/intro/IntroPart.java
@@ -0,0 +1,86 @@
+package com.infineon.cv.intro;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.IPropertyListener;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.intro.IIntroPart;
+import org.eclipse.ui.intro.IIntroSite;
+
+public class IntroPart implements IIntroPart {
+
+	@Override
+	public Object getAdapter(Class adapter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IIntroSite getIntroSite() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(IIntroSite site, IMemento memento)
+			throws PartInitException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void standbyStateChanged(boolean standby) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void saveState(IMemento memento) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addPropertyListener(IPropertyListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Image getTitleImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removePropertyListener(IPropertyListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
